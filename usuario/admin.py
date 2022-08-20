4515f8001d1a8623054f87be1194a3b52be18c6c
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-08-19 21:54:22 -0300

    validando cadastro

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Usuarios
 
-admin.site.register(Usuarios)
+@admin.register(Usuarios)
+class usuarioAdmin(admin.ModelAdmin):
+    readonly_fields= ('nome', 'email', 'senha', 'idade')
--- usuario/views.py
+++ usuario/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import Usuarios
+from hashlib import sha256
 
 def cadastro(request):
     return render(request, 'cadastro.html')
@@ -6,3 +10,28 @@ def cadastro(request):
 def login(request):
     return render(request, 'login.html')
 
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    idade = request.POST.get('idade')
+
+    usuario = Usuarios.objects.filter(email = email)
+
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('cadastro/?status=1')
+    
+    if len(senha) < 6:
+        return redirect('cadastro/?status=2')
+
+    if len(usuario) > 0:
+        return redirect('cadastro/?status=3')
+    
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        usuario = Usuarios(nome = nome, email = email, senha = senha, idade = idade)
+        usuario.save()
+
+        return redirect('cadastro/?status=0')
+    except:
+        return redirect('cadastro/?status=4')
