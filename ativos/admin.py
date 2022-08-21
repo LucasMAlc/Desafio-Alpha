b9a70a29950619c893ea58005d9135f5e642e24c
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-08-21 02:26:09 -0300

    models

--- ativos/admin.py
+++ ativos/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Ativos
 
+admin.site.register(Ativos)
+
+''' 
+Somente para leitura no admin:
+
 @admin.register(Ativos)
 class ativosAdmin(admin.ModelAdmin):
     readonly_fields= ('sigla', 'usuario')
+
+'''
