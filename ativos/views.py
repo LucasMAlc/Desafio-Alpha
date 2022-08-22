941bcbf8c94688f585d75ff8f108826c5884e388
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-08-21 21:34:31 -0300

    pegando cotação

--- ativos/views.py
+++ ativos/views.py
@@ -1,33 +1,68 @@
+from hmac import compare_digest
+from tracemalloc import start
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from usuario.models import Usuarios
 from pandas_datareader import data as web
 import pandas as pd
-import yfinance as yf
 from .models import Ativos
+from datetime import datetime
+from .forms import CadastroAtivos
 
+def inicio(request):
+    return redirect('/usuario/login/')
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuarios.objects.get(id = request.session['usuario'])
-        siglas = Ativos.objects.filter(usuario = usuario)
-        '''lmt = yf.Ticker("LMT")
-        hist = lmt.history(period="30d",  interval = "60m")
-        return HttpResponse(f"olá {usuario} historico da lmt: {hist['Open'], hist['Close']}")'''
-        return render(request, 'home.html', {'siglas': siglas})
+
+        ativos = Ativos.objects.filter(usuario = usuario)
+        form = CadastroAtivos()
+        form.fields['usuario'].initial = request.session['usuario']
+
+        for ativo in ativos:
+            x = web.DataReader(ativo.sigla, data_source='yahoo', start='08/19/2022', end=datetime.now())
+            x = pd.DataFrame(x)
+            print('valor atual de: ', ativo.sigla, 'é: ', x['Close'])
+            if(x['Close'].iloc[0] > ativo.preco_venda):
+                print('vende', ativo.sigla)
+
+            elif(x['Close'].iloc[0] < ativo.preco_compra):
+                print('compra', ativo.sigla)
+            else:
+                print('valor igual')
+                
+
+
+        return render(request, 'home.html', {'ativos': ativos,
+                                             'usuario_logado': request.session.get('usuario'),
+                                             'form': form,
+                                             })
+
     else:
         return redirect('/usuario/login/?status=2')
     
 
 def sobre(request):
-    return render(request, 'sobre.html')
+    return render(request, 'sobre.html', {'usuario_logado': request.session.get('usuario')})
 
 
 def user(request):
     if request.session.get('usuario'):
         usuario = Usuarios.objects.get(id = request.session['usuario'])
-        return render(request, 'user.html', {'usuario': usuario})
+        return render(request, 'user.html', {'usuario': usuario, 'usuario_logado': request.session.get('usuario')})
     else:
         return redirect('/usuario/login/?status=2')
-    
 
+def cadastrar_ativo(request):
+    if request.method =='POST':
+        form = CadastroAtivos(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/home/')
+        else:
+            return HttpResponse("dados inválidos")
+
+def excluir_ativo(request):
+    return HttpResponse('excluido')     
